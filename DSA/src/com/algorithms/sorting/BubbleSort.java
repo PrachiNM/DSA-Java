bb793feda5a98e89d9ee23a0f593a3168387e0da
PrachiNM <meharepn@gmail.com>
2021-09-24 17:18:04 +0530

    Bubble Sort implementation

---
+++ DSA/src/com/algorithms/sorting/BubbleSort.java
@@ -0,0 +1,41 @@
+package com.algorithms.sorting;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class BubbleSort {
+
+    public void bubbleSort(int[] arr, int n){
+        if(n == 0 || n == 1)
+            return;
+        boolean flag;
+        int temp;
+        int n2 = n;
+        for(int j=0; j<n; j++){
+            flag = false;
+            for(int i=0; i<n2-1; i++){
+                if(arr[i]>arr[i+1]){
+                     temp = arr[i];
+                     arr[i] = arr[i+1];
+                     arr[i+1] = temp;
+                     flag = true;
+                }
+            }
+            if(!flag)
+                return;
+            n2--;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // Enter size of array
+        int n = sc.nextInt();
+        int[] array = new int[n];
+        // Enter elements of array
+        for(int i=0;  i<n; i++)
+            array[i] = sc.nextInt();
+        new BubbleSort().bubbleSort(array, n);
+        System.out.println(Arrays.toString(array));
+    }
+}
